766e16ea8da5d9a17f8a780b75d989754da13dc1
Tomoya Tsuji <1020tomoya@gmail.com>
2015-08-10 15:35:30 +0900

    Scrolling appears on ViewController

--- comicScroll_SoundEffect/ViewController.swift
+++ comicScroll_SoundEffect/ViewController.swift
@@ -9,17 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var scrollView : UIScrollView!
+    
+    var pHeight:CGFloat = 240.0
+    var pWidth:CGFloat = 375.0
+    
+    let pNum:Int = 22
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for var i=0;i < pNum; i++ {
+            var n:Int = i+1
+            //UIImageのインスタンスを作成
+            var image:UIImage! = UIImage(named:"\(n).png")
+            //UIImageの初期化
+            var imageView = UIImageView(image: image)
+            var rect:CGRect = imageView.frame
+            rect.size.height = pHeight
+            rect.size.width = pWidth
+            imageView.frame = rect
+            imageView.tag = n
+            //UIImagwViewをViewに追加
+            self.scrollView.addSubview(imageView)
+        }
+        setupScrollImages()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+    }
+    
+    func setupScrollImages(){
+        var imageDummy:UIImage! = UIImage(named: "1.png")
+        var imgView = UIImageView(image: imageDummy)
+        var subview:Array = scrollView.subviews
+        
+        var px:CGFloat = 0.0
+        var py:CGFloat = 100.0
+        
+        for var i=0; i<subview.count; i++ {
+            imgView = subview[i] as! UIImageView
+            if (imgView.isKindOfClass(UIImageView) && imgView.tag > 0){
+                var viewFrame:CGRect = imgView.frame
+                viewFrame.origin = CGPointMake(px, py)
+                imgView.frame = viewFrame
+                px += (pWidth)
+            }
+        }
+        var nWidth:CGFloat = pWidth*CGFloat(pNum)
+        scrollView.contentSize = CGSizeMake(nWidth, pHeight)
     }
 
-
 }
+
 
